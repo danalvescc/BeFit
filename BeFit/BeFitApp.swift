c05f152d3deaf3443401b12e7eca6b753713e79e
Daniel Alves <danalvescc@gmail.com>
2023-05-10 12:18:00 -0300

    Initial Commit

---
+++ BeFit/BeFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeFitApp.swift
+//  BeFit
+//
+//  Created by Daniel  Alves Barreto on 10/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BeFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
